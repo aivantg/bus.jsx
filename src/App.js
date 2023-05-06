2e9bf79547e21b91f37b193f678aec16ea0c0e10
aivantg <aivant.developer@gmail.com>
2023-05-06 01:21:18 -0700

    Added new bus icon and courtney face easter egg

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
-import {render} from 'react-dom';
-import logo from './logo.svg';
-import './App.css';
-import ListRoutes from './ListRoutes.jsx';
-import InfoPanel from './InfoPanel.jsx';
-import Info from './Info.jsx';
+import React, { Component } from "react";
+import { render } from "react-dom";
+import logo from "./logo.svg";
+import "./App.css";
+import ListRoutes from "./ListRoutes.jsx";
+import InfoPanel from "./InfoPanel.jsx";
+import Info from "./Info.jsx";
 import StopList from "./StopList.jsx";
-import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
-import 'semantic-ui/dist/semantic.min.css';
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
+import "./../node_modules/fomantic-ui/dist/semantic.min.css";
 
 const AC_TRANSIT_API_BASE_URL = "https://api.actransit.org/transit/";
 
@@ -15,24 +15,40 @@ export class MapContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-                    buses: null,
-                    current_route: null,
-                    current_bus_info: null,
-                    stops: null
-                  };
+      buses: null,
+      current_route: null,
+      current_bus_info: null,
+      stops: null,
+      useCourtneyIcon: false,
+    };
+    window.addEventListener("keyup", (e) => {
+      console.log("keyup");
+      console.log(e.key);
+      if (e.key === " " || e.code === "Space" || e.keyCode === 32) {
+        this.setState((prevState) => ({
+          useCourtneyIcon: !prevState.useCourtneyIcon,
+        }));
+      }
+    });
   }
 
   async fetchBuses() {
     let route = this.state.current_route;
     // Fetches buses for a specified route from the AC Transit API and updates the buses in state
     if (route) {
-      let url = AC_TRANSIT_API_BASE_URL + "route/" + route + "/vehicles/?token=" + process.env.REACT_APP_AC_TRANSIT_API_KEY;
+      let url =
+        AC_TRANSIT_API_BASE_URL +
+        "route/" +
+        route +
+        "/vehicles/?token=" +
+        process.env.REACT_APP_AC_TRANSIT_API_KEY;
       try {
         let response = await fetch(url);
         let responseJSON = await response.json();
-        this.setState({buses: responseJSON});
-      }
-      catch (error) {
+        console.log(responseJSON);
+        this.setState({ buses: responseJSON });
+      } catch (error) {
+        console.log(error);
         // TODO: Make into a more noticeable failure messsage
         console.log("The API seems to be down right now. Try again later!");
       }
@@ -44,15 +60,25 @@ export class MapContainer extends Component {
     let bus = this.state.current_bus_info["CurrentTripId"];
     // Fetches buses for a specified route from the AC Transit API and updates the buses in state
     if (route) {
-      let url = AC_TRANSIT_API_BASE_URL + "route/" + route + "/trip/" + bus + "/stops/?token=" + process.env.REACT_APP_AC_TRANSIT_API_KEY;
+      let url =
+        AC_TRANSIT_API_BASE_URL +
+        "route/" +
+        route +
+        "/trip/" +
+        bus +
+        "/stops/?token=" +
+        process.env.REACT_APP_AC_TRANSIT_API_KEY;
       let response = await fetch(url);
       let responseJSON = await response.json();
-      this.setState({stops: responseJSON});
+      this.setState({ stops: responseJSON });
     }
   }
 
   componentDidMount() {
-    this.interval = setInterval(() => this.fetchBuses(this.state.current_route), 5000);
+    this.interval = setInterval(
+      () => this.fetchBuses(this.state.current_route),
+      5000
+    );
   }
 
   componentWillUnmount() {
@@ -61,76 +87,88 @@ export class MapContainer extends Component {
 
   updateCurrentRoute(route) {
     // Updates the current_route in state and then calls fetchBuses to get new bus locations
-    this.setState({current_route: route,
-                    stops: null}, this.fetchBuses);
-
+    this.setState({ current_route: route, stops: null }, this.fetchBuses);
   }
 
   onMarkerClick(busInfo) {
-    this.setState({current_bus_info: busInfo}, this.getStops);
+    this.setState({ current_bus_info: busInfo }, this.getStops);
   }
 
   render() {
-    const {google} = this.props;
+    const { google } = this.props;
 
     let info_style = {
       width: "25%",
-    }
+    };
 
     let map_style = {
       width: "70%",
-      float: "right"
+      float: "right",
     };
 
     let top_level_style = {
-      display: "flex"
-    }
+      display: "flex",
+    };
 
     return (
       <div style={top_level_style}>
         <div style={info_style}>
           <div>
-            <ListRoutes onClick={(route) => this.updateCurrentRoute(route)} />
+            <ListRoutes
+              onClick={(route) => this.updateCurrentRoute(route)}
+              onHeaderClick={() => {
+                this.setState((prevState) => ({
+                  useCourtneyIcon: !prevState.useCourtneyIcon,
+                }));
+              }}
+            />
           </div>
           <div>
             <InfoPanel>
-                <div label="Info">
-                    <Info busInfo={this.state.current_bus_info} stops={this.state.stops} currentRoute={this.state.current_route} />
-                </div>
-                <div label="Stops">
-                    <StopList stops={this.state.stops} />
-                </div>
+              <div label="Info">
+                <Info
+                  busInfo={this.state.current_bus_info}
+                  stops={this.state.stops}
+                  currentRoute={this.state.current_route}
+                />
+              </div>
+              <div label="Stops">
+                <StopList stops={this.state.stops} />
+              </div>
             </InfoPanel>
           </div>
         </div>
         <div style={map_style}>
-          <Map google={this.props.google}
-          initialCenter={{
-            lat: 37.8719,
-            lng: -122.2585
-          }}
-          zoom={13}>
+          <Map
+            google={this.props.google}
+            initialCenter={{
+              lat: 37.8719,
+              lng: -122.2585,
+            }}
+            zoom={13}
+          >
             {
               // Maps the bus route data passed in
               this.state.buses &&
-              this.state.buses.map(b =>
-                (
-                  <Marker onClick={(e) => this.onMarkerClick(b)}
-                          key={b.CurrentTripId}
-                          position={{lat: b.Latitude, lng: b.Longitude}}
-                          icon={{
-                            url: "http://www.clipartroo.com/images/104/green-bus-stop-clipart-104115.png",
-                            scaledSize: new google.maps.Size(20,20)
-                          }}/>
-                )
-              )
+                this.state.buses.map((b) => (
+                  <Marker
+                    onClick={(e) => this.onMarkerClick(b)}
+                    key={b.CurrentTripId}
+                    position={{ lat: b.Latitude, lng: b.Longitude }}
+                    icon={{
+                      url: this.state.useCourtneyIcon
+                        ? "courtney_icon.png"
+                        : "bus_icon.png",
+                      scaledSize: new google.maps.Size(20, 20),
+                    }}
+                  />
+                ))
             }
 
-
             <InfoWindow onClose={this.onInfoWindowClose}>
-                <div>
-                  <h1>Placeholder for state</h1>
-                </div>
+              <div>
+                <h1>Placeholder for state</h1>
+              </div>
             </InfoWindow>
           </Map>
         </div>
@@ -141,4 +179,4 @@ export class MapContainer extends Component {
 
 export default GoogleApiWrapper({
   apiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
-})(MapContainer)
+})(MapContainer);
--- src/ListRoutes.jsx
+++ src/ListRoutes.jsx
@@ -1,23 +1,63 @@
 import React from "react";
-import Route from './Route.jsx';
+import Route from "./Route.jsx";
 
 // props: onClick = this.updateCurrentRoute
-const ListRoutes = props => {
-
-    return (
-        <div>
-        <h1 className="ui header" style={{textAlign: "center", padding: "10px"}}>BUS ROUTES</h1>
-            <div className="ui vertical buttons">
-                    <div onClick={() => props.onClick("6")}><Route route="6" active="false" desc="Downtown Oakland/Downtown Berkeley"/></div>
-                    <div onClick={() => props.onClick("36")}><Route route="36" active="false" desc="West Oakland/UC Berkeley"/></div>
-                    <div onClick={() => props.onClick("51B")}><Route route="51B" active="false" desc="Rockridge/Berkeley Amtrak"/></div>
-                    <div onClick={() => props.onClick("18")}><Route route="18" active="false" desc="San Pablo/Lake Merritt"/></div>
-                    <div onClick={() => props.onClick("52")}><Route route="52" active="false" desc="UC Village/UC Campus"/></div>
-                    <div onClick={() => props.onClick("65")}><Route route="65" active="false" desc="Spruce St./Euclid Ave."/></div>
-                    <div onClick={() => props.onClick("F")}><Route route="F" active="false" desc="San Francisco/Berkeley"/></div>
-            </div>
+const ListRoutes = (props) => {
+  return (
+    <div>
+      <h1
+        className="ui header"
+        style={{ textAlign: "center", padding: "10px" }}
+        onClick={() => props.onHeaderClick()}
+      >
+        BUS ROUTES
+      </h1>
+      <div
+        style={{
+          width: "100%",
+          display: "flex",
+          justifyContent: "center",
+          marginBottom: "10px",
+        }}
+      >
+        <button onClick={() => props.onHeaderClick()}>Click to smile :)</button>
+      </div>
+      <div className="ui vertical buttons">
+        <div onClick={() => props.onClick("6")}>
+          <Route
+            route="6"
+            active="false"
+            desc="Downtown Oakland/Downtown Berkeley"
+          />
         </div>
-    );
+        <div onClick={() => props.onClick("36")}>
+          <Route route="36" active="false" desc="West Oakland/UC Berkeley" />
+        </div>
+        <div onClick={() => props.onClick("51B")}>
+          <Route route="51B" active="false" desc="Rockridge/Berkeley Amtrak" />
+        </div>
+        <div onClick={() => props.onClick("18")}>
+          <Route route="18" active="false" desc="San Pablo/Lake Merritt" />
+        </div>
+        <div onClick={() => props.onClick("52")}>
+          <Route route="52" active="false" desc="UC Village/UC Campus" />
+        </div>
+        <div onClick={() => props.onClick("65")}>
+          <Route route="65" active="false" desc="Spruce St./Euclid Ave." />
+        </div>
+        <div onClick={() => props.onClick("F")}>
+          <Route route="F" active="false" desc="San Francisco/Berkeley" />
+        </div>
+        <div onClick={() => props.onClick("801")}>
+          <Route
+            route="801"
+            active="false"
+            desc="E. 14th - Mission All Nighter"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ListRoutes;
